d2740955d697a4a3d14e077e705f022f4352ca19
truerep <business.truerep@gmail.com>
2025-08-21 00:47:58 +0530

    env

--- backend/server.js
+++ backend/server.js
@@ -22,9 +22,10 @@ app.use(helmet());
 
 // CORS configuration
 app.use(cors({
-  origin: process.env.NODE_ENV === 'production' 
-    ? [process.env.FRONTEND_URL || 'https://your-frontend-domain.com'] 
-    : ['http://localhost:3000'],
+  origin: ["https://fly-destination-ui.vercel.app", "https://flydestination.co.in", "http://localhost:3000"]
+  // origin: process.env.NODE_ENV === 'production' 
+  //   ? [process.env.FRONTEND_URL || 'https://your-frontend-domain.com'] 
+  //   : ['http://localhost:3000'],
   credentials: true
 }));
 
